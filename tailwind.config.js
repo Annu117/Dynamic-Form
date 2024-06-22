ecae64725c3ed1e3f57ac738824ddf36976589de
Annu Kumari <108427028+Annu117@users.noreply.github.com>
2024-06-23 03:06:02 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Inter', 'Roboto', 'sans-serif'],
       },
       colors: {
         primary: '#3b82f6',
